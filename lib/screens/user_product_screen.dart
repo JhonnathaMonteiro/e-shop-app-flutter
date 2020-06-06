350aa499508dd8fabd12473efed74db4f3aff7df
JhonnathaMonteiro <jhonnatha.am@gmail.com>
2020-06-06 10:13:38 -0300

    added form validation

--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/user_product_item.dart';
+import './edit_product_screen.dart';
 
 class UserProductScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -18,7 +19,7 @@ class UserProductScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              //...
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
             },
           ),
         ],
